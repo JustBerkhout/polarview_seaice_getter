f36903a3358800023f26f52302532c9004049734
Just Berkhout <just.berkhout@utas.edu.au>
2017-12-01 10:43:17 +1100

    remove LOG reference

--- apsg.sh
+++ apsg.sh
@@ -7,8 +7,8 @@ ACQ_MONTH=${ACQ_DATE:0:4}${ACQ_DATE:5:2}
 TEMP_DIR=${BASH_SOURCE%/*}/tmp
 DEST_DIR=${BASH_SOURCE%/*}/downloads
 
-echo "ACQ_DATE = $ACQ_DATE" >> $LOG_FILE
-echo "ACQ_MONTH = $ACQ_MONTH" >> $LOG_FILE
+echo "ACQ_DATE = $ACQ_DATE" 
+echo "ACQ_MONTH = $ACQ_MONTH" 
 
 wget "http://geos.polarview.aq/geoserver/polarview/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=polarview:vw_last200s1subsets&srsName=EPSG:4326&outputFormat=csv&cql_filter=acqtime>="$ACQ_DATE"T00:00:00.000 AND acqtime<="$ACQ_DATE"T23:59:59.999 AND BBOX(geom, 819391.62, 1419227.92, 2262268.85, -3918364.59)"  -O - | tail -n +2 | awk -F "\"*,\"*" '{print $2}' |  sed -e 's/.tif//g' > $TEMP_DIR/basenames
 
